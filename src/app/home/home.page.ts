ea87b5084f390e8709c9f3076a03ea293cc2d0c5
Maxwell Hinman <mhinman87@gmail.com>
2025-03-08 07:05:55 -0600

    Updated homepage design and fixed header scrolling issue

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faRobot, faUserTie, faCalendarCheck } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule]
+  imports: [IonicModule, FontAwesomeModule]
 })
 export class HomePage {
+
+  aiIcon = faRobot;
+  expertIcon = faUserTie;
+  bookingIcon = faCalendarCheck;
+  
+
+ 
+  
 
   constructor() {}
 
